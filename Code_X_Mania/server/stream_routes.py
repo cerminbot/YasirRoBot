212b238744f8e05fd6150a3b0f06d1e1d58d5cf8
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-14 11:26:16 +0700

    Update stream_routes.py

--- Code_X_Mania/server/stream_routes.py
+++ Code_X_Mania/server/stream_routes.py
@@ -55,7 +55,7 @@ async def old_stream_handler(request):
         
 @routes.get("/unduh/{message_id}")
 @routes.get("/unduh/{message_id}/")
-@routes.get(r"/unduh/{message_id:\d+}/{name:\d+}?hash={hashed}")
+@routes.get(r"/unduh/{message_id:\d+}/{name}")
 async def stream_handler(request):
     try:
         message_id = int(request.match_info['message_id'])
