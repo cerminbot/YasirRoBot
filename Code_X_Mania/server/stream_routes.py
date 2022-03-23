a67a7305344ca09d802cd1dc0fb4355f4d04bb8f
yasirarism <55983182+yasirarism@users.noreply.github.com>
2022-03-23 21:03:59 +0700

    Update stream_routes.py

--- Code_X_Mania/server/stream_routes.py
+++ Code_X_Mania/server/stream_routes.py
@@ -36,11 +36,12 @@ async def root_route_handler(request):
 
 @routes.get("/lk21/{judul}")
 async def lk21_api(request):
+       title = request.match_info['judul']
        headers = {
            'User-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
        }
 
-       html = await getcontent(f"https://149.56.24.226/?s=soul")
+       html = await getcontent(f"https://149.56.24.226/?s={title}")
        soup = BeautifulSoup(html, 'lxml')
        data = []
        for res in soup.find_all(class_='search-item'):
